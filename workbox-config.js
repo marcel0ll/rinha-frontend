d1ade3fe1a895f028eafd5440962ce8f7909f6ee
marcel0ll <marcelo@lotuz.dev>
2023-10-25 14:48:30 -0300

    2 font files

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'src/',
 	globPatterns: [
-		'**/*.{html,js,css,ttf}'
+		'**/*.{html,js,css,ttf,png}'
 	],
 	swDest: 'src/sw.js',
 	ignoreURLParametersMatching: [
