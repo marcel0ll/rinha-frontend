dab5d493f1dab8d881e12cfe91207c9bee06eef4
marcel0ll <marcelo@lotuz.dev>
2023-10-25 15:04:23 -0300

    ttf -> woff2

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'src/',
 	globPatterns: [
-		'**/*.{html,js,css,ttf,png}'
+		'**/*.{html,js,css,woff2,png}'
 	],
 	swDest: 'src/sw.js',
 	ignoreURLParametersMatching: [
